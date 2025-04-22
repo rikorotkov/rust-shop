38c602583720b34918a6b1b379c9a8ffba927e7e
Roman <googlik999@yandex.ru>
2025-04-22 15:21:17 +0300

    create model donation

---
+++ src/main/java/ru/floda/home/rustshop/model/Donation.java
@@ -0,0 +1,37 @@
+package ru.floda.home.rustshop.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "donations")
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
+
+    @CreationTimestamp
+    private LocalDateTime donationDate;
+
+    enum PaymentStatus {
+        DONE, ERROR, IN_PROGRESS
+    }
+}
---
+++ src/main/java/ru/floda/home/rustshop/model/User.java
@@ -0,0 +1,18 @@
+package ru.floda.home.rustshop.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
