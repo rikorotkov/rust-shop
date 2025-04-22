bad01381fd43173de990e8c3f44aff5c0dfc99f6
Roman <googlik999@yandex.ru>
2025-04-22 14:02:13 +0300

    git init

---
+++ src/main/java/ru/floda/home/rustshop/RustShopApplication.java
@@ -0,0 +1,13 @@
+package ru.floda.home.rustshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RustShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RustShopApplication.class, args);
+    }
+
+}
