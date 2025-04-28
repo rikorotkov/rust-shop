cc436a0c31771b7b05b33212f398a2a4d05f56e9
Roman <googlik999@yandex.ru>
2025-04-28 19:18:32 +0300

    add rcon button and style status

--- src/main/java/ru/floda/home/rustshop/controller/ServerController.java
+++ src/main/java/ru/floda/home/rustshop/controller/ServerController.java
@@ -1,36 +1,36 @@
-    package ru.floda.home.rustshop.controller;
+package ru.floda.home.rustshop.controller;
 
-    import lombok.RequiredArgsConstructor;
-    import org.springframework.stereotype.Controller;
-    import org.springframework.ui.Model;
-    import org.springframework.web.bind.annotation.*;
-    import ru.floda.home.rustshop.model.Server;
-    import ru.floda.home.rustshop.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.floda.home.rustshop.model.Server;
+import ru.floda.home.rustshop.service.ServerService;
 
-    @Controller
-    @RequiredArgsConstructor
-    @RequestMapping("/user/{userId}/servers")
-    public class ServerController {
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user/{userId}/servers")
+public class ServerController {
 
-        private final ServerService serverService;
+    private final ServerService serverService;
 
-        @GetMapping
-        public String getServers(@PathVariable long userId, Model model) {
-            model.addAttribute("servers", serverService.findAllServers(userId));
-            model.addAttribute("userId", userId);
-            return "server/index";
-        }
-
-        @GetMapping("/new")
-        public String showFormNewServer(@PathVariable long userId, Model model) {
-            model.addAttribute("server", new Server());
-            model.addAttribute("userId", userId);
-            return "server/new";
-        }
-
-        @PostMapping("/new")
-        public String processNewServer(@ModelAttribute("server") Server server, @ModelAttribute("userId") long userId) {
-            serverService.addNewServer(server, userId);
-            return "redirect:/user/" + userId + "/servers";
-        }
+    @GetMapping
+    public String getServers(@PathVariable long userId, Model model) {
+        model.addAttribute("servers", serverService.findAllServers(userId));
+        model.addAttribute("userId", userId);
+        return "server/index";
     }
+
+    @GetMapping("/new")
+    public String showFormNewServer(@PathVariable long userId, Model model) {
+        model.addAttribute("server", new Server());
+        model.addAttribute("userId", userId);
+        return "server/new";
+    }
+
+    @PostMapping("/new")
+    public String processNewServer(@ModelAttribute("server") Server server, @ModelAttribute("userId") long userId) {
+        serverService.addNewServer(server, userId);
+        return "redirect:/user/" + userId + "/servers";
+    }
+}
