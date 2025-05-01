0c274ba3d9ebdccce166a4ca46aa6866619f8c66
Roman <googlik999@yandex.ru>
2025-05-01 19:17:20 +0300

    web rcon client

--- src/main/java/ru/floda/home/rustshop/service/RconService.java
+++ src/main/java/ru/floda/home/rustshop/service/RconService.java
@@ -1,34 +1,15 @@
 package ru.floda.home.rustshop.service;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.floda.home.rustshop.exceptions.AuthenticationException;
-import ru.floda.home.rustshop.model.Server;
-import ru.floda.home.rustshop.rcon.Rcon;
 import ru.floda.home.rustshop.repository.ServerRepository;
 
-import java.io.IOException;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RconService {
 
-    private static final Logger log = LoggerFactory.getLogger(RconService.class);
     private final ServerRepository serverRepository;
 
-    public void test(Server server) {
-        try {
-            Rcon rcon = new Rcon(server);
-            String res = rcon.command("list");
-            log.info(res);
-        } catch (AuthenticationException e) {
-            log.error("Authentication failed", e);
-        } catch (IOException e) {
-            log.error("Connection error", e);
-        } catch (Exception e) {
-            log.error("Unexpected error", e);
-        }
-    }
 }
